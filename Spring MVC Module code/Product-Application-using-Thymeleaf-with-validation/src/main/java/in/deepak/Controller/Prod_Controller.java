c42d09f170296d0e45999aa4f1523bc7ecd4d8e4
Deepak Singh <deepaksingh.desire@gmail.com>
2024-10-05 21:43:54 +0530

    add

--- Spring MVC Module code/Product-Application-using-Thymeleaf-with-validation/src/main/java/in/deepak/Controller/Prod_Controller.java
+++ Spring MVC Module code/Product-Application-using-Thymeleaf-with-validation/src/main/java/in/deepak/Controller/Prod_Controller.java
@@ -5,6 +5,8 @@ import in.deepak.Prod_Interface.ProdInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +33,11 @@ public class Prod_Controller {
 
             //-------------------------------  To save the data into h2 database  --------------------------------------------------------------
             @PostMapping("/product")
-            public String saveProduct (@ModelAttribute ("product") Prod_Entity prodEntity , Model model  ){
+            public String saveProduct (@Validated @ModelAttribute ("product") Prod_Entity prodEntity , BindingResult result, Model model  ){
+
+                if(result.hasErrors()){
+                    return "index";
+                }
 
                 Prod_Entity save = pro.save(prodEntity);
 
--- Spring MVC Module code/Product-Application-using-Thymeleaf-with-validation/src/main/java/in/deepak/Entity/Prod_Entity.java
+++ Spring MVC Module code/Product-Application-using-Thymeleaf-with-validation/src/main/java/in/deepak/Entity/Prod_Entity.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 
@@ -15,7 +19,17 @@ public class Prod_Entity {
     @Id
     @GeneratedValue
     Integer id;
+
+
+    @NotBlank(message = "Name is Mandatory")
+    @Size (min = 3 , max = 15 , message = "name must be Between 3 t0 15 characters")
     String name;
+
+    @NotNull (message = "Price is Mandatory")
+    @Positive (message = "Price should be positive number or greater then 0")
     Integer price;
+
+    @NotNull (message = "Quantity is Mandatory")
+    @Positive (message = "Price should be positive number or greater then 0")
     Integer quantity;
 }
