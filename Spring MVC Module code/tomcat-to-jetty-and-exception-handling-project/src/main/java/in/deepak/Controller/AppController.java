9bd52c428a4f7517971c9943df84bbbc1d04da65
Deepak Singh <deepaksingh.desire@gmail.com>
2024-10-09 21:06:47 +0530

    add

--- Spring MVC Module code/tomcat-to-jetty-and-exception-handling-project/src/main/java/in/deepak/Controller/AppController.java
+++ Spring MVC Module code/tomcat-to-jetty-and-exception-handling-project/src/main/java/in/deepak/Controller/AppController.java
@@ -3,8 +3,10 @@ package in.deepak.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AppController {
 
 
@@ -15,7 +17,13 @@ public class AppController {
         return "index";
     }
 
-
+    @GetMapping("/welcome")
+    @ResponseBody
+    public String welcome (Model model){
+        String text = "hello";
+        model.addAttribute("msg" , text );
+        return "how are you";
+    }
 
 
 }
